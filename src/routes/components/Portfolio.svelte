07925abb9a0197c12c900c19d268095ebbb473f3
paradoxicalinsomniac <81902126+micolarighi@users.noreply.github.com>
2024-01-23 23:40:42 +0700

    fix link

--- src/routes/components/Portfolio.svelte
+++ src/routes/components/Portfolio.svelte
@@ -2,7 +2,8 @@
 	<a
 		href="https://micolarighi-portfolio.vercel.app/"
 		target="_blank"
-		class="bg-blue-500 cursor-pointer rounded py-2 px-3 border-2 border-blue-400 hover:bg-blue-600 transition ease-in-out"
-		><button class="w-full align-middle"> Buka Portofolio</button></a
+		class="bg-blue-500 cursor-pointer rounded py-2 px-3 border-2 border-blue-400 hover:bg-blue-600 transition ease-in-out block"
+	>
+		Buka Portofolio</a
 	>
 </div>
