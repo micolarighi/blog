400796cbec4a769e63e70c07b9905948c7174720
paradoxicalinsomniac <micolaarighi@gmail.com>
2024-01-25 16:10:22 +0700

    fix background styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 <div class="min-h-screen bg-gradient-to-tr from-gray-900 from-80% via-blue-900 to-blue-800">
 	<div class="px-12 pt-12 text-center">
 		<h1 class="md:text-6xl text-4xl font-bold">Micola Arighi</h1>
-		<h4 class="md:text-2xl text-lg leading-tight">
+		<h4 class="md:text-2xl text-lg leading-tight mt-2">
 			Seorang pemuda yang <span class="text-blue-400">hobi ngoding & suka sharing</span>
 		</h4>
 	</div>
