70942d4ed07fc5434a9e15fe990ec5ae6567845a
paradoxicalinsomniac <81902126+micolarighi@users.noreply.github.com>
2024-02-12 01:34:25 +0700

    update loader

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 {#if $navigating}
-	<div class="w-screen h-screen flex justify-center items-center bg-gray-900">
+	<div class="flex justify-center items-center bg-gray-900">
 		<Jumper size="100" color="#3b82f6" unit="px" duration="0.5s" />
 	</div>
 {/if}
